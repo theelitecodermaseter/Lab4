99effc18e74efc2e18ff58072a965877e70517e6
chris-sherman-sp <85285506+chris-sherman-sp@users.noreply.github.com>
2022-05-07 18:09:01 +0800

    Fix issue with HAL library paths

--- src/PiDemo.py
+++ src/PiDemo.py
@@ -9,8 +9,11 @@ from time import sleep
 #from ..hal import hal_dc_motor as dc_motor
 
 
-from src.hal import  hal_lcd as LCD
-
+from hal import hal_led as led
+from hal import hal_lcd as LCD
+from hal import hal_dc_motor as dc_motor
+from hal import hal_buzzer as buzzer
+from hal import hal_servo as servo
 
 
 def blink_led():
@@ -50,25 +53,20 @@ def main():
     lcd = LCD.lcd()
 
     sleep(0.5)
-    LCD.backlight(0)  # turn backlight off
+    lcd.backlight(0)  # turn backlight off
 
     sleep(0.5)
-    LCD.backlight(1)  # turn backlight on
+    lcd.backlight(1)  # turn backlight on
 
-    LCD.lcd_clear()
-    LCD.lcd_display_string("Hi welcome to", 1)  # write on line 1
-    LCD.lcd_display_string("DevOps for AIoT", 2)  # write on line 2
+    lcd.lcd_clear()
+    lcd.lcd_display_string("Hi welcome to", 1)  # write on line 1
+    lcd.lcd_display_string("DevOps for AIoT", 2)  # write on line 2
     # starting on 3rd column
 
     sleep(2)  # wait 2 sec
 
     # Get IP address
     local_ip_address = socket.gethostbyname("raspberrypi")
-
-    # Display current Raspbery Pi IP Address
-    LCD.lcd_clear()
-    LCD.lcd_display_string("IP Address: ", 1)
-    LCD.lcd_display_string(local_ip_address, 2)
 
     # Buzzer beep
     buzzer.init()
